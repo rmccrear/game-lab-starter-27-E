b7c563fbb423a0855c4bd517d0feb59f6319905a
Jarnell Williams <a.jwillz225@gmail.com.fr>
2025-08-08 01:34:22 -0500

    anichanges

--- js/user-controls.js
+++ js/user-controls.js
@@ -1,5 +1,12 @@
 
 function respondToUser() {
+if ((log1.x) >= 400) 
+    log1.x = 0;
+    log1.y = randomNumber(10, 400);
 
+    if ((log2.y) >= 400) 
+    log2.y = 0;
+    log2.x = randomNumber(10, 400);
+  
 
 }
