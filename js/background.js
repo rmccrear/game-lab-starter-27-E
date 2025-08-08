d184c4abe81d72d9118a19b48b986fd6bad9ed30
Jo Greenlee <sweettroubles29@gmail.com>
2025-08-07 19:16:16 -0500

    background

--- js/background.js
+++ js/background.js
@@ -4,16 +4,24 @@ function drawBackground() {
 }
 
 function background1() {
-    background("darkBlue");
+    background("darkgreen"); // Jungle sky color
     noStroke();
-    fill(255, 255, 0);
-    ellipse(randomNumber(0, 400), randomNumber(0, 400), 3, 3);
-    ellipse(randomNumber(0, 400), randomNumber(0, 400), 3, 3);
-    ellipse(340, 50, 60, 60);
-    fill(0, 0, 139);
-    ellipse(320, 30, 60, 60);
-  }
 
-  function background2() {
-    
-  }
+    // Jungle floor
+    fill("saddlebrown");
+    rect(0, height - 50, width, 50); // Jungle floor
+
+    // Simple trees (trunks and canopies)
+    fill("saddlebrown");
+    rect(100, height - 150, 20, 100); // Tree trunk 1
+    rect(300, height - 150, 20, 100); // Tree trunk 2
+
+    fill("forestgreen");
+    ellipse(110, height - 170, 80, 80); // Tree canopy 1
+    ellipse(310, height - 170, 80, 80); // Tree canopy 2
+}
+
+
+function background2() {
+
+}
