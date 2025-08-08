750aaee94769687dfee5c77d635a9388b47da12b
Jarnell Williams <a.jwillz225@gmail.com.fr>
2025-08-08 02:09:52 -0500

    movesave

--- js/sprite-interactions.js
+++ js/sprite-interactions.js
@@ -6,4 +6,13 @@ function doSpriteInteraction() {
     banana.x=randomNumber(0,400);
     banana.y=randomNumber(0,400);}
    
+     if (monkey.isTouching(log1)) {
+    log1.displace(monkey);}
+
+     if (monkey.isTouching(log2)) {
+    log2.displace(monkey);
+    
+  }
+    
+  
 }
--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -1,8 +1,18 @@
 
 function doSpriteMovement () {
-
-
-  
-
+  log1.velocityX = 5;
+ if ((log1.x) >= 400) {
+    log1.x = 0;
+    log1.y = randomNumber(10, 400);
+  }
+log2.velocityY = 5;
+  if ((log2.y) >= 400) {
+    log2.y = 0;
+    log2.x = randomNumber(10, 400);
+  }
+banana.velocityY = 5;
+if (banana.y< 5 || banana.y>400) {
+    banana.x = randomNumber(25, 375);
+    banana.y = 0;
 }
-
+  }
