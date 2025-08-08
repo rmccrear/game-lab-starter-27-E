8a498ffe621eeb6c33ffe54683e4f0e2de6402b1
Jarnell Williams <a.jwillz225@gmail.com.fr>
2025-08-07 19:53:24 -0500

    resized monkey image to be smaller

--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -2,5 +2,14 @@
 function doSpriteMovement() {
     // Example Code follows.
     // Please delete and replace with your own code.
-   
+   if (monkeyAnimation.y >= 320) 
+    monkeyAnimation.velocityY = 0;
 }
+if (keyDown("UP_ARROW")) {
+    monkeyAnimation.velocityY = -5;
+  }
+
+ if ((monkeyAnimation.y) <= 150) {
+    monkeyAnimation.velocityY = monkeyAnimation.velocityY + 0.25;
+  }
+
