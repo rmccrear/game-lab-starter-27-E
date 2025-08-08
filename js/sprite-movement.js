ee46ae9614b1d131d6ac562d0f9ed30da483e609
Jarnell Williams <a.jwillz225@gmail.com.fr>
2025-08-08 01:41:20 -0500

    controlchange

--- js/sprite-movement.js
+++ js/sprite-movement.js
@@ -2,23 +2,7 @@
 function doSpriteMovement () {
 
 
-if (keyDown("LEFT_ARROW")) {
-    monkey.velocityX = -5;
-    
-  }
-  // Move right
-if (keyDown("RIGHT_ARROW")) {
-    monkey.velocityX = 5;
-    
-  }
-
-  // Move up
-  if (keyDown(UP_ARROW)) {
-    
-    monkey.velocityY = -5;
   
-  }
-monkey.velocityY = monkey.velocityY + 0.25;
-  drawSprites();
+
 }
 
--- js/user-controls.js
+++ js/user-controls.js
@@ -1,12 +1,23 @@
 
 function respondToUser() {
-if ((log1.x) >= 400) 
-    log1.x = 0;
-    log1.y = randomNumber(10, 400);
 
-    if ((log2.y) >= 400) 
-    log2.y = 0;
-    log2.x = randomNumber(10, 400);
+
+if (keyDown("LEFT_ARROW")) {
+    monkey.velocityX = -5;
+    
+  }
+  // Move right
+if (keyDown("RIGHT_ARROW")) {
+    monkey.velocityX = 5;
+    
+  }
+
+  // Move up
+  if (keyDown(UP_ARROW)) {
+    
+    monkey.velocityY = -5;
   
-
+  }
+monkey.velocityY = monkey.velocityY + 0.25;
+  drawSprites();
 }
