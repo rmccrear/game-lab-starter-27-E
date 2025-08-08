d20661273a826c1bd0a69fdcbbd5a6cf20ee4e0a
Jo Greenlee <sweettroubles29@gmail.com>
2025-08-07 21:25:05 -0500

    background

--- js/background.js
+++ js/background.js
@@ -1,6 +1,11 @@
 
 function drawBackground() {
     background1();
+    if (score < 10) {
+        background1();
+    } else {
+        background2();
+    }
 }
 
 function background1() {
@@ -11,14 +16,9 @@ function background1() {
     rect(50, 0, 30, 400);
     rect(320, 0, 30, 400);
 
-    // Leaves (big canopy)
+    // Leaves 
     fill("limegreen");
     ellipse(65, 50, 150, 100);
     ellipse(335, 50, 150, 100);
     ellipse(200, 0, 300, 150);
-}
-
-
-function background2() {
-
 }
